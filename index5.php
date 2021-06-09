257b6916362dc0f35c45fd9460a45c83f1dd0351
Nick Jurevich <44304306+Smesh292@users.noreply.github.com>
2021-06-09 13:05:31 +0300

    Update index5.php

--- index5.php
+++ index5.php
@@ -1,31 +1,33 @@
 <?php
-//Step1
- $db = mysqli_connect('localhost','root','','fakeexpert')
- or die('Error connecting to MySQL server.');
+	//Step1
+	$db = mysqli_connect('localhost','root','','fakeexpert')
+	or die('Error connecting to MySQL server.');
 ?>
 
 <html>
- <head>
- </head>
- <body>
- <h1>PHP connect to MySQL</h1>
+<head>
+</head>
+<body>
+<h1>PHP connect to MySQL</h1>
 
-  <?php
-  //Step2
-  $query = "SELECT * FROM records ORDER BY time";
-  mysqli_query($db, $query) or die('Error querying database.');
+<?php
+	//Step2
+	$query = "SELECT * FROM records ORDER BY time";
+	mysqli_query($db, $query) or die('Error querying database.');
 
-  //Step3
-  $result = mysqli_query($db, $query);
-  $row = mysqli_fetch_array($result);
+	//Step3
+	$result = mysqli_query($db, $query);
+	$row = mysqli_fetch_array($result);
 
-  while ($row = mysqli_fetch_array($result)) {
-   echo $row['id'] . ' ' . $row['playerid'] . ': ' . $row['partnerid'] . ' ' . $row['time'] .'<br />';}
-  //Step 4
-  mysqli_close($db);
-  ?>
+	while ($row = mysqli_fetch_array($result))
+	{
+		echo $row['id'] . ' ' . $row['playerid'] . ': ' . $row['partnerid'] . ' ' . $row['time'] . ' ' . $row['map'] . ' ' . $row['date'] . '<br />';
+	}
+	//Step 4
+	mysqli_close($db);
+?>
 
- </body>
+</body>
 </html>
 
 //https://stackoverflow.com/questions/9494209/how-to-link-mysql-to-html
