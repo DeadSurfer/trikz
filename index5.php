dc39cee5d126430e89a0b62a6c3e6539af9c0d5a
Nick Jurevich <44304306+Smesh292@users.noreply.github.com>
2021-06-09 15:03:09 +0300

    Update index5.php

--- index5.php
+++ index5.php
@@ -24,7 +24,7 @@
 			mysqli_query($db, $query2) or die('Error querying in table.');
 			$result2 = mysqli_query($db, $query2);
 			$row2 = mysqli_fetch_array($result2);
-			echo $row2['username'] . ' ';
+			//echo $row2['username'] . ' ';
 			//$row2 = mysqli_fetch_field($result2);
 			//while ($row2 = mysqli_fetch_array($result2))
 			{
@@ -32,7 +32,7 @@
 				mysqli_query($db, $query3) or die('Error querying in table.');
 				$result3 = mysqli_query($db, $query3);
 				$row3 = mysqli_fetch_array($result3);
-				echo $row3['username'] . ' ';
+				//echo $row3['username'] . ' ';
 				//$row3 = mysqli_fetch_field($result3);
 				//while($row3 = mysqli_fetch_array($result3))
 				{
@@ -45,7 +45,7 @@
 			//echo $row['id'] . ' ' . $row['playerid'] . ' ' . $row['partnerid'] . ' ' . $row['time'] . ' ' . $row['map'] . ' ' . $row['date'] . '<br />';
 			$formatedDate = date("Y-m-d H:i:s", (int)$row['date']);
 			//echo $row['time'] . ' ' . $row['map'] . ' ' . $row['date'] . '<br>'; //https://code-boxx.com/format-unix-timestamp-date-time-php/#:~:text=We%20can%20use%20the%20date%20function%20to%20format,date%20%28%22D%2C%20j%20F%20Y%20h%3Ai%3As%20A%22%2C%20%24UNIX%29%3B
-			echo $row['time'] . ' ' . $row['map'] . ' ' . $formatedDate . '<br>';
+			echo $row2['username'] . ' ' . $row3['username'] . ' ' . $row['time'] . ' ' . $row['map'] . ' ' . $formatedDate . '<br>';
 		}
 		//Step 4
 		mysqli_close($db);
