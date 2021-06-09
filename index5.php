f7b94ef018f2e8990dfbecf4199e066c6780e58d
Nick Jurevich <44304306+Smesh292@users.noreply.github.com>
2021-06-09 13:06:20 +0300

    Update index5.php

--- index5.php
+++ index5.php
@@ -5,29 +5,28 @@
 ?>
 
 <html>
-<head>
-</head>
+<head>Trikz Timer</head>
 <body>
-<h1>PHP connect to MySQL</h1>
+	<h1>PHP connect to MySQL</h1>
 
-<?php
-	//Step2
-	$query = "SELECT * FROM records ORDER BY time";
-	mysqli_query($db, $query) or die('Error querying database.');
+	<?php
+		//Step2
+		$query = "SELECT * FROM records ORDER BY time";
+		mysqli_query($db, $query) or die('Error querying database.');
 
-	//Step3
-	$result = mysqli_query($db, $query);
-	$row = mysqli_fetch_array($result);
+		//Step3
+		$result = mysqli_query($db, $query);
+		$row = mysqli_fetch_array($result);
 
-	while ($row = mysqli_fetch_array($result))
-	{
-		echo $row['id'] . ' ' . $row['playerid'] . ': ' . $row['partnerid'] . ' ' . $row['time'] . ' ' . $row['map'] . ' ' . $row['date'] . '<br />';
-	}
-	//Step 4
-	mysqli_close($db);
-?>
+		while ($row = mysqli_fetch_array($result))
+		{
+			echo $row['id'] . ' ' . $row['playerid'] . ': ' . $row['partnerid'] . ' ' . $row['time'] . ' ' . $row['map'] . ' ' . $row['date'] . '<br />';
+		}
+		//Step 4
+		mysqli_close($db);
+	?>
 
-</body>
+	</body>
 </html>
 
 //https://stackoverflow.com/questions/9494209/how-to-link-mysql-to-html
