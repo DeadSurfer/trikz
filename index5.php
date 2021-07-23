ced14a34e3804ee3af7c93237e29587592b4053f
Nick Jurevich <44304306+Smesh292@users.noreply.github.com>
2021-07-23 14:49:04 +0300

    Update index5.php

--- index5.php
+++ index5.php
@@ -259,12 +259,12 @@
 		//$countx++;
 		$serverRecord = 0;
 		$query0 = "SELECT COUNT(id) FROM records WHERE map = '$name'";
-		//mysqli_query($db, $query0) or die('Error querying in table. [2]');
+		mysqli_query($db, $query0) or die('Error querying in table. [2]');
 		//$result2 = mysqli_query($db, $query0);
 		//$row0 = mysqli_fetch_array($result0);
 		//$row0 = mysqli_fet
 		//$num = 
-		$row0 = $db->query("SELECT COUNT(id) FROM records WHERE map = '$name'")->fetchColumn();
+		$row0 = $db->query0("SELECT COUNT(id) FROM records WHERE map = '$name'")->fetchColumn();
 		while($row = mysqli_fetch_assoc($result))
 		{
 			$query2 = "SELECT username FROM users WHERE steamid = $row[playerid]";
