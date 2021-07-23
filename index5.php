5587634cd747d122c0bbc8eaa18e74f9a9590d41
Nick Jurevich <44304306+Smesh292@users.noreply.github.com>
2021-07-23 13:16:00 +0300

    Update index5.php

--- index5.php
+++ index5.php
@@ -275,18 +275,23 @@
 					$hours = floor($row[time] / 3600);
 					$mins = floor($row[time] / 60 % 60);
 					$secs = floor($row[time] % 60);
-					$time = sprintf('%02d:%02d:%02d', $hours, $mins, $secs);
-					
+					$time = sprintf("%02d:%02d:%02d", $hours, $mins, $secs);
+					$timeDiff;
 					if($serverRecord == 0)
 					{
 						$serverRecord = $row[time];
 					}
+					$timeDiff = $row[time] - $serverRecord;
+					$timeDiffHours = floor($timeDiff / 3600);
+					$timeDiffMins = floor($timeDiff / 60 % 60);
+					$timeDiffSecs = floor($timeDiff % 60);
+					$timeDiffFormated = sprintf("%02d:%02d:%02d", $timeDiffHours, $timeDiffMins, $timeDiffSecs);
 					$formatedDateYmd = date("Y-m-d", (int)$row[date]);
 					$formatedDateHis = date("H:i:s", (int)$row[date]);
 					//if($count == 1)
 						//echo "<tr><td><center>$count</center></td><td>$row2[username] [U:1:$row[playerid]]<br>$row3[username] [U:1:$row[partnerid]]</td><td><center>$time</center></td><td><center>$row[completions]</center></td><td><center>$formatedDateYmd<br>$formatedDateHis</center></td></tr>";
 					//else
-					echo "<tr><td><center>$count</center></td><td>$row2[username] [U:1:$row[playerid]]<br>$row3[username] [U:1:$row[partnerid]]</td><td class='active-row'><center>$time</center></td><td><center>$row[completions]</center></td><td><center>$formatedDateYmd<br>$formatedDateHis</center></td></tr>";
+					echo "<tr><td><center>$count</center></td><td>$row2[username] [U:1:$row[playerid]]<br>$row3[username] [U:1:$row[partnerid]]</td><td class='active-row'><center>$time</center></td> (+$timeDiffFormated)<td><center>$row[completions]</center></td><td><center>$formatedDateYmd<br>$formatedDateHis</center></td></tr>";
 					//$countx = $countx + 1;
 					$count++;
 					//echo "<td>$row2x[username]</td>";
