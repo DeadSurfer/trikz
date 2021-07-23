2a992ad6b1a16bc76d48bcb97c21908f48a1c82a
Nick Jurevich <44304306+Smesh292@users.noreply.github.com>
2021-07-23 13:29:09 +0300

    Update index5.php

--- index5.php
+++ index5.php
@@ -291,7 +291,7 @@
 					//else
 					//https://www.w3schools.com/html/html_colors.asp
 					//https://www.tutorialspoint.com/html/html_colors.htm
-					echo "<tr><td><center>$count</center></td><td>$row2[username] [U:1:$row[playerid]]<br>$row3[username] [U:1:$row[partnerid]]</td><td class='active-row'><center>$time<font color='#980000'>(+$timeDiffFormated)</font></center></td><td><center>$row[completions]</center></td><td><center>$formatedDateYmd<br>$formatedDateHis</center></td></tr>";
+					echo "<tr><td><center>$count</center></td><td>$row2[username] [U:1:$row[playerid]]<br>$row3[username] [U:1:$row[partnerid]]</td><td class='active-row'><center>$time <font color='#980000'>(+$timeDiffFormated)</font></center></td><td><center>$row[completions]</center></td><td><center>$formatedDateYmd<br>$formatedDateHis</center></td></tr>";
 					//$countx = $countx + 1;
 					$count++;
 					//echo "<td>$row2x[username]</td>";
